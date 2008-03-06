03dcb64378327416a651ca98ff94d562ad4c6c15
Thorhallur Helgason <laddi@idega.com>
2008-03-06 15:25:18 +0000

    *** empty log message ***

--- src/java/com/idega/block/survey/presentation/SurveyEditorWindow.java
+++ src/java/com/idega/block/survey/presentation/SurveyEditorWindow.java
@@ -6,20 +6,29 @@ import com.idega.presentation.ui.Window;
 public class SurveyEditorWindow extends Window {
 
 	static final String PRM_INSTANCE_ID = "sew_p_id";
-	
+
 	public SurveyEditorWindow() {
 		super(600, 700);
 		setScrollbar(true);
 		setAllMargins(0);
 	}
-	
+
 	public void main(IWContext iwc) {
 		String sInst = iwc.getParameter(PRM_INSTANCE_ID);
 		if (sInst != null) {
-			SurveyEditor se = new SurveyEditor(Integer.parseInt(sInst));
-			se.maintainParameter(PRM_INSTANCE_ID, sInst);
-			add(se);
-		} else {
+			String mode = iwc.getParameter(Survey.PRM_SWITCHTO_MODE);
+			if (mode.equals(Survey.MODE_EDIT)) {
+				SurveyEditor se = new SurveyEditor(Integer.parseInt(sInst));
+				se.maintainParameter(PRM_INSTANCE_ID, sInst);
+				add(se);
+			}
+			else if (mode.equals(Survey.MODE_RESULTS)) {
+				SurveyResultEditor sre = new SurveyResultEditor();
+				sre.setICObjectInstanceID(Integer.parseInt(sInst));
+				add(sre);
+			}
+		}
+		else {
 			add("No instance ID");
 		}
 	}
