0670ca6a706b2b2359efc1c4b98f2dafb97c2224
Thorhallur Helgason <laddi@idega.com>
2008-03-06 15:55:13 +0000

    *** empty log message ***

--- src/java/com/idega/block/survey/presentation/SurveyEditorWindow.java
+++ src/java/com/idega/block/survey/presentation/SurveyEditorWindow.java
@@ -17,13 +17,13 @@ public class SurveyEditorWindow extends Window {
 		String sInst = iwc.getParameter(PRM_INSTANCE_ID);
 		if (sInst != null) {
 			String mode = iwc.getParameter(Survey.PRM_SWITCHTO_MODE);
-			if (mode.equals(Survey.MODE_EDIT)) {
+			if (mode != null && mode.equals(Survey.MODE_EDIT)) {
 				SurveyEditor se = new SurveyEditor(Integer.parseInt(sInst));
 				se.maintainParameter(PRM_INSTANCE_ID, sInst);
 				se.maintainParameter(Survey.PRM_SWITCHTO_MODE, Survey.MODE_EDIT);
 				add(se);
 			}
-			else if (mode.equals(Survey.MODE_RESULTS)) {
+			else {
 				SurveyResultEditor sre = new SurveyResultEditor();
 				sre.setICObjectInstanceID(Integer.parseInt(sInst));
 				add(sre);
